d6baba45770879d47ad596d1b3c50e4e83c65a82
Chris Hanson <christopher.james.hanson@gmail.com>
2017-01-21 10:45:12 -0800

    Update CreeRelays.ino

--- Arduino/Sketches/CreeRelays/CreeRelays.ino
+++ Arduino/Sketches/CreeRelays/CreeRelays.ino
@@ -1,48 +1,8 @@
 //******************************************************************************************
-//  File: CreeRelays.ino
-//  Author: Dan G Ogorchock 
-//
-//  Summary:  This Arduino Sketch demonstrates the ability of one Arduino + a Cree Lightbulb 
-//            zigbee module to implement a multiple output custom device for integration into
-//            SmartThings.  This sketch was created using an Arduino Nano clone from ebay ($4)
-//            and a Cree Connected Lightbulb's Zigbee module.
-//
-//            Using the Cree Connected LEBD bulb's tiny zigbee module, the sketch interprets
-//            the various dim levels into multiple dicrete value ranges.  Using these discrete
-//            ranges, one can take whatever action is desired.  For example, one could turn on
-//            a different relay (i.e. digital output) for each of the discrete dim value ranges.
-//
-//            In this example, DIM levels 1 through 8 will result in Digital Outputs 2 through 9
-//            being turned on/off respectively.  Use your imagination as there are many possibilities
-//            and combinations you can use to control devices.  Current design allows only 1 output
-//            to be turned on at a time.
-//          
-//            Since the Cree Zigbee module uses a pulse width modulated output propotional to 
-//            dim level, I decided to create a very accurate PWM timing routine to attempt to
-//            achieve 100 discrete value ranges that correspong to the 0 to 99 dim levels available
-//            from the SmartThings dimmer. This timing circuit utilizes the Arduino's TIMER2 to 
-//            provide the resolution and precision needed to properly discern all 100 levels.
-//
-//  Change History:
-//
-//    Date        Who            What
-//    ----        ---            ----
-//    2015-04-18  Dan Ogorchock  Original Creation
-//
-//******************************************************************************************
-
-
-//******************************************************************************************
 // Pin Definitions
 //******************************************************************************************
 #define PIN_RELAY_1    2
 #define PIN_RELAY_2    3
-#define PIN_RELAY_3    4
-#define PIN_RELAY_4    5
-#define PIN_RELAY_5    6
-#define PIN_RELAY_6    7
-#define PIN_RELAY_7    8
-#define PIN_RELAY_8    9
 
 #define PIN_CREE_PWM   11 //NOTE:  This PIN is hard-coded in the ISR routine below.  Defined here for informational purposes only.
 #define PIN_CREE_ONOFF 12
@@ -65,21 +25,9 @@ void setup()
   
   pinMode(PIN_RELAY_1, OUTPUT);
   pinMode(PIN_RELAY_2, OUTPUT);
-  pinMode(PIN_RELAY_3, OUTPUT);
-  pinMode(PIN_RELAY_4, OUTPUT);
-  pinMode(PIN_RELAY_5, OUTPUT);
-  pinMode(PIN_RELAY_6, OUTPUT);
-  pinMode(PIN_RELAY_7, OUTPUT);
-  pinMode(PIN_RELAY_8, OUTPUT);
   
-  digitalWrite(PIN_RELAY_1, LOW);
-  digitalWrite(PIN_RELAY_2, LOW);
-  digitalWrite(PIN_RELAY_3, LOW);
-  digitalWrite(PIN_RELAY_4, LOW);
-  digitalWrite(PIN_RELAY_5, LOW);
-  digitalWrite(PIN_RELAY_6, LOW);
-  digitalWrite(PIN_RELAY_7, LOW);
-  digitalWrite(PIN_RELAY_8, LOW);
+  digitalWrite(PIN_RELAY_1, HIGH);
+  digitalWrite(PIN_RELAY_2, HIGH);
   
   setupTimingInterrupt();
 }
@@ -96,19 +44,19 @@ void loop()
   if (level != lastLevel)
   {
     lastLevel = level;
+    
     Serial.print(F("Cree Level = "));
     Serial.println(level);
     
      //TODO: Add any code needed to handle the value of the PWM input 0 through 99
     
-    digitalWrite(PIN_RELAY_1, level == 1?HIGH:LOW);
-    digitalWrite(PIN_RELAY_2, level == 2?HIGH:LOW);
-    digitalWrite(PIN_RELAY_3, level == 3?HIGH:LOW);
-    digitalWrite(PIN_RELAY_4, level == 4?HIGH:LOW);
-    digitalWrite(PIN_RELAY_5, level == 5?HIGH:LOW);
-    digitalWrite(PIN_RELAY_6, level == 6?HIGH:LOW);
-    digitalWrite(PIN_RELAY_7, level == 7?HIGH:LOW);
-    digitalWrite(PIN_RELAY_8, level == 8?HIGH:LOW);
+    digitalWrite(PIN_RELAY_1, level == 1?LOW:HIGH);
+    digitalWrite(PIN_RELAY_2, level == 2?LOW:HIGH);
+
+    delay(250);
+
+    digitalWrite(PIN_RELAY_1, HIGH);
+    digitalWrite(PIN_RELAY_2, HIGH);
   }
   
   //Get the value of the Cree Zigbee board's On/Off digital ouput
@@ -200,4 +148,3 @@ void setupTimingInterrupt()
   TIMSK2 |= (1 << OCIE2A);// enable timer compare interrupt
   interrupts();
 }
-
